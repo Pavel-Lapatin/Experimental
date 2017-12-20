301ec2ec760337213a5d678b32fc6c5d68eb383b
Pavel Lapatin <plapatin@gmail.com>
2017-12-20 08:54:38 +0300

    mend

---
+++ FileManager/NetMastery.Lab05.FileManager.DAL/UnitOfWork/GenericUnitOfWorkWithOneRepo/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetMastery.Lab05.FileManager.DAL.Interfacies
+{
+    public interface IUnitOfWork<T> : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : class;
+        bool HasPendingChanges { get; }
+        RegisterNew(T item) ;
+        void RegisterChanged(T item);
+        void RegisterRemoved(T item);
+        void Unregister(T item);
+        void Rollback();
+        Task CommitAsync(CancellationToken cancellationToken);
+    }
+}
+
---
+++ FileManager/NetMastery.Lab05.FileManager.DAL/UnitOfWork/GenericUnitOfWorkWithOneRepo/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using NetMastery.Lab05.FileManager.DAL.Interfacies;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace NetMastery.Lab05.FileManager.DAL.Repository
+{
+    
+    public class UnitOfWork<TEntity> : IUnitOfWork
+    {
+        private readonly IEqualityComparer<T> comparer;
+        private readonly HashSet<T> inserted;
+        private readonly HashSet<T> updated;
+        private readonly HashSet<T> deleted;
+        protected UnitOfWork()
+        protected UnitOfWork(IEqualityComparer<T> comparer)
+    protected IEqualityComparer<T> Comparer { get; }
+        protected virtual ICollection<T> InsertedItems { get; }
+        protected virtual ICollection<T> UpdatedItems { get; }
+        protected virtual ICollection<T> DeletedItems { get; }
+        public virtual bool HasPendingChanges { get; }
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+    protected virtual void AcceptChanges()
+    protected abstract bool IsNew(T item)
+    public virtual void RegisterNew(T item)
+    public virtual void RegisterChanged(T item)
+    public virtual void RegisterRemoved(T item)
+    public virtual void Unregister(T item)
+    public virtual void Rollback()
+    public abstract Task CommitAsync(CancellationToken cancellationToken);
+    }
+}
